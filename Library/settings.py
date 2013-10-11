6fe0387f2f3df3e7df32132258d882764ef33f1d
jnphilipp <jnphilipp@gmail.com>
2013-10-11 15:28:23 +0200

    Setting up database infos.

--- Library/settings.py
+++ Library/settings.py
@@ -13,14 +13,14 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2'
+		'NAME': 'dj_library',
+		'USER': 'library',
+		'PASSWORD': 'library',
+		'HOST': '',
+		'PORT': '',
+	}
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
