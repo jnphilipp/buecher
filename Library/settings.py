a083a683d41b55ffbf18ae660e92320c9e113195
jnphilipp <jnphilipp@gmail.com>
2013-10-28 19:47:38 +0100

    Updates settings.

--- Library/settings.py
+++ Library/settings.py
@@ -64,7 +64,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -72,7 +72,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-	('assets', os.path.join(PROJECT_PATH, 'static')), 
+	(os.path.join(PROJECT_PATH, 'assets')), 
 )
 
 # List of finder classes that know how to find static files in
@@ -114,6 +114,7 @@ WSGI_APPLICATION = 'Library.wsgi.application'
 
 TEMPLATE_DIRS = (
 	os.path.join(PROJECT_PATH, 'templates'),
+	'/var/www/Library/templates',
 )
 
 INSTALLED_APPS = (
