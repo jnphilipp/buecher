72d58be0077763aae82d47f04d1aae4b2b92b47c
jnphilipp <jnphilipp@gmail.com>
2013-10-31 19:22:29 +0100

    Fix for duplicate entries after order_by.

--- books/views.py
+++ books/views.py
@@ -57,7 +57,7 @@ def index(request):
 	search = request.GET.get('search')
 	if search:
 		firstname, _, lastname = search.rpartition(' ')
-		book_list = Book.objects.filter(Q(title__icontains=search) | Q(series__name__icontains=search) | (Q(authors__firstname__icontains=firstname) & Q(authors__lastname__icontains=lastname)) | Q(publisher__name__icontains=search)).order_by('authors__lastname', 'authors__firstname', 'series__name', 'volume', 'published_on')
+		book_list = Book.objects.filter(Q(title__icontains=search) | Q(series__name__icontains=search) | (Q(authors__firstname__icontains=firstname) & Q(authors__lastname__icontains=lastname)) | Q(publisher__name__icontains=search)).order_by('authors__lastname', 'authors__firstname', 'series__name', 'volume', 'published_on').distinct()
 	else:
 		book_list = Book.objects.all().order_by('-updated_at')
 	paginator = Paginator(book_list, 30)
