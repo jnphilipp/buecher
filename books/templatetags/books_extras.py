6dc0ef31b5efacf4d3216c0f35d4eb622c01f58e
jnphilipp <jnphilipp@gmail.com>
2014-02-12 21:16:45 +0100

    Updated detail template.

--- books/templatetags/books_extras.py
+++ books/templatetags/books_extras.py
@@ -1,4 +1,5 @@
 from django.template import Library
+from django.utils.numberformat import format
 
 register = Library()
 
@@ -9,3 +10,11 @@ def is_false(arg):
 @register.filter
 def list_title(obj, epk):
 	return obj.get_list_title(epk)
+
+@register.filter
+def floatdot(value, decimal_pos=2):
+	if not value:
+		return format(0, ",", decimal_pos)
+	else:
+		return format(value, ",", decimal_pos)
+floatdot.is_safe = True
