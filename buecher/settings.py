014983a8a73ff21c20425b6e2a7b9e72603b938e
jnphilipp <jnphilipp@gmail.com>
2014-04-27 22:58:01 +0200

    Updated settings.

--- buecher/settings.py
+++ buecher/settings.py
@@ -1,24 +1,73 @@
-# Django settings for buecher project.
+"""
+Django settings for buecher project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from ConfigParser import RawConfigParser
 from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 config = RawConfigParser()
+config.optionxform = str
 config.read(BASE_DIR + '/buecher/settings.ini')
 
-DEBUG = config.get('debug','DEBUG')
-TEMPLATE_DEBUG = config.get('debug','TEMPLATE_DEBUG')
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config.get('secrets','SECRET_KEY')
+SESSION_COOKIE_SECURE = config.getboolean('secrets','SESSION_COOKIE_SECURE')
+CSRF_COOKIE_SECURE = config.getboolean('secrets','CSRF_COOKIE_SECURE')
+SESSION_EXPIRE_AT_BROWSER_CLOSE = config.getboolean('secrets','SESSION_EXPIRE_AT_BROWSER_CLOSE')
 
-ADMINS = (
-	('jnphilipp', 'jnphilipp@gmail.com'),
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config.getboolean('debug','DEBUG')
+TEMPLATE_DEBUG = config.getboolean('debug','TEMPLATE_DEBUG')
+
+ALLOWED_HOSTS = config.get('host','ALLOWED_HOSTS').split()
+ADMINS = tuple(config.items('admins'))
+
+EMAIL_USE_TLS = config.getboolean('host_email','EMAIL_USE_TLS')
+DEFAULT_FROM_EMAIL = config.get('host_email','DEFAULT_FROM_EMAIL')
+SERVER_EMAIL = config.get('host_email','SERVER_EMAIL')
+EMAIL_HOST = config.get('host_email','EMAIL_HOST')
+EMAIL_PORT = config.getint('host_email','EMAIL_PORT')
+EMAIL_HOST_USER = config.get('host_email','EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config.get('host_email','EMAIL_HOST_PASSWORD')
+
+
+# Application definition
+INSTALLED_APPS = (
+	'suit',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
+	'south',
+	'books',
 )
 
-MANAGERS = ADMINS
+MIDDLEWARE_CLASSES = (
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
 
+
+ROOT_URLCONF = 'buecher.urls'
+WSGI_APPLICATION = 'buecher.wsgi.application'
+
+
+# Database
 DATABASES = {
 	'default': {
 		'ENGINE': config.get('database', 'DATABASE_ENGINE'),
@@ -30,140 +79,51 @@ DATABASES = {
 	}
 }
 
-# Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en/1.4/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
+# Internationalization
+LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Europe/Berlin'
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
-
-SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
 USE_L10N = True
-
-# If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-
-# Additional locations of static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
 	os.path.join(BASE_DIR, 'assets'),
 )
-
-# List of finder classes that know how to find static files in
-# various locations.
 STATICFILES_FINDERS = (
 	'django.contrib.staticfiles.finders.FileSystemFinder',
 	'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#	'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = config.get('secrets','SECRET_KEY')
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-	'django.template.loaders.filesystem.Loader',
-	'django.template.loaders.app_directories.Loader',
-#	'django.template.loaders.eggs.Loader',
-)
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-TEMPLATE_CONTEXT_PROCESSORS = TCP + (
-	'django.core.context_processors.request',
-)
 
-MIDDLEWARE_CLASSES = (
-	'django.middleware.common.CommonMiddleware',
-	'django.contrib.sessions.middleware.SessionMiddleware',
-	'django.middleware.csrf.CsrfViewMiddleware',
-	'django.contrib.auth.middleware.AuthenticationMiddleware',
-	'django.contrib.messages.middleware.MessageMiddleware',
-	# Uncomment the next line for simple clickjacking protection:
-	#'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-ROOT_URLCONF = 'buecher.urls'
-
-# Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'buecher.wsgi.application'
-
+# Templates
 TEMPLATE_DIRS = (
 	os.path.join(BASE_DIR, 'templates'),
 )
-
-INSTALLED_APPS = (
-	'django.contrib.auth',
-	'django.contrib.contenttypes',
-	'django.contrib.sessions',
-	'django.contrib.messages',
-	'django.contrib.staticfiles',
-	'suit',
-	'django.contrib.admin',
-	'south',
-	'books',
+TEMPLATE_LOADERS = (
+	'django.template.loaders.filesystem.Loader',
+	'django.template.loaders.app_directories.Loader',
 )
+TEMPLATE_CONTEXT_PROCESSORS = TCP + (
+	'django.core.context_processors.request',
+	'django.contrib.messages.context_processors.messages',
+)
+
 
 SUIT_CONFIG = {
 	'ADMIN_NAME':'buecher',
 	'LIST_PER_PAGE': 50,
-}
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-	'version': 1,
-	'disable_existing_loggers': False,
-	'filters': {
-		'require_debug_false': {
-			'()': 'django.utils.log.RequireDebugFalse'
-		}
-	},
-	'handlers': {
-		'mail_admins': {
-			'level': 'ERROR',
-			'filters': ['require_debug_false'],
-			'class': 'django.utils.log.AdminEmailHandler'
-		}
-	},
-	'loggers': {
-		'django.request': {
-			'handlers': ['mail_admins'],
-			'level': 'ERROR',
-			'propagate': True,
-		},
-	}
+	'MENU': (
+		{'label': 'buecher', 'icon':'logo', 'url': '/'},
+		'auth',
+		'books',
+	),
 }
