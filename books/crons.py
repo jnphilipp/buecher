310d03667931db24b7fb4bf5fe49fcd453f25365
jnphilipp <jnphilipp@gmail.com>
2014-11-22 19:38:56 +0100

    Added django_cron and cronjob for full cirlce magazine.

---
+++ books/crons.py
@@ -0,0 +1,11 @@
+from django.core import management
+from django_cron import CronJobBase, Schedule
+
+class FullCircleMagazineJob(CronJobBase):
+	RUN_AT_TIMES = ['12:00']
+
+	schedule = Schedule(run_at_times=RUN_AT_TIMES)
+	code = 'books.fullcirclemagazine'
+
+	def do(self):
+		management.call_command('fullcircle')
