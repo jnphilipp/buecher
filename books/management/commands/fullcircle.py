3132f347b30f4388ee9b55e6972e04be2b8b9322
jnphilipp <jnphilipp@gmail.com>
2015-04-01 22:00:50 +0200

    Fixed fullcricle epub name.

--- books/management/commands/fullcircle.py
+++ books/management/commands/fullcircle.py
@@ -50,7 +50,7 @@ class Command(BaseCommand):
 					urlretrieve('http://dl.fullcirclemagazine.org/issue%d_en.pdf' % volume, pdf)
 					EBookFile.objects.create(ebook_file=join('books', str(book.id), name), book=book)
 
-					name = slugify('Issue %s' % volume) + '.epub'
+					name = slugify('Issue %d' % volume) + '.epub'
 					epub = join(folder, name)
 					urlretrieve('http://dl.fullcirclemagazine.org/issue%d_en.epub' % volume, epub)
 					EBookFile.objects.create(ebook_file=join('books', str(book.id), name), book=book)
