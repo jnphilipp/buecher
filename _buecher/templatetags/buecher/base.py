ad1ef63463f4fcc133f9463568d28d345d6d535c
jnphilipp <me@jnphilipp.org>
2015-07-12 22:07:12 +0200

    Added app _buecher.

---
+++ _buecher/templatetags/buecher/base.py
@@ -0,0 +1,35 @@
+from _buecher.templatetags.buecher import register
+
+@register.filter
+def decrement(value):
+    return int(value) - 1
+
+@register.filter
+def increment(value):
+    return int(value) + 1
+
+@register.filter
+def lookup(d, key):
+    return d[key] if key in d else None
+
+@register.filter
+def startswith(value, start):
+    return value.startswith(start)
+
+@register.filter
+def previous(value, arg):
+    try:
+        return value[int(arg) - 1] if int(arg) - 1 != -1 else None
+    except:
+        return None
+
+@register.filter
+def next(value, arg):
+    try:
+        return value[int(arg) + 1]
+    except:
+        return None
+
+@register.filter(name='addcss')
+def addcss(field, css):
+    return field.as_widget(attrs={"class":css})
