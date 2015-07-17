33a8106ff1fe6aec9f2ca8267e884352f7f7f742
jnphilipp <me@jnphilipp.org>
2015-07-17 17:35:31 +0200

    Updated book/edition templates.

--- books/views/book.py
+++ books/views/book.py
@@ -7,7 +7,4 @@ def books(request):
 
 def book(request, slug):
     book = get_object_or_404(Book, slug=slug)
-    if book.editions.count() == 1:
-        return redirect('edition', slug=book.slug, edition_id=book.editions.first().id)
-    else:
-        return render(request, 'buecher/books/book/book.html', locals())
+    return render(request, 'buecher/books/book/book.html', locals())
